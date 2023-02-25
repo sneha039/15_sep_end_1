d7c5376f308818b3b0ed4c40b4100666a074eb99
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-02-25 20:54:18 +0530

    Add files via upload

---
+++ sep_Method_and_end_Method.py
@@ -0,0 +1,43 @@
+"""  sep method  """
+
+# print("Hello" , "World")
+# print("Hello"  "World")  
+# print("Hello"+'-'+"World") 
+# print("Hello"  "World",'Core', 'Python',sep="")
+# print("Hello"  "World",'Core', 'Python',sep="-")
+# print("Hello" , "World",'Core', 'Python',sep="-",)
+# print("Hello" , "World",'Core', 'Python',sep="-ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-"+ "ok")
+# print("Hello" , "World",'Core', 'Python',sep="-" ' ' "ok")
+
+
+"""  end method  """
+
+# print(1,end='.')
+# print(2)
+# print(3)
+# print("Hello" , "Python",end=".")
+# print("Hello" , "Python",end="-")
+# print("New Version is 3.11.0")
+# print("ok fine")
+
+# print("Hello" , "Python",end="-")
+# print("New Version is 3.11.0",end=" ")
+# print("ok fine")
+
+
+""" Combination of end and sep method  """
+
+# print("World" , "CUP" , end="." , sep='')
+# print("World" , "CUP" , end="." , sep='-')
+# print("core" , "programming" , "python",sep="@", end="#")
+
+# print("Python" *5) 
+# print("Python " *5) 
+# print("Python\n" *5) 
+# print("Python\n" *5,end="") 
+
+# print("hello" , "world", sep=" 123 ")
+
+# print('HD\n' * 4,end='')
